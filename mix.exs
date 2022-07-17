4e6122a4f1a7de37aadacd0ad5d23eb7736ab091
Amorésio <a.junior.srlopes@gmail.com>
2022-07-17 07:29:52 -0400

    add libs

--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule DesafioCotacao.MixProject do
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -40,7 +47,9 @@ defmodule DesafioCotacao.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:tesla, "~> 1.4"},
       {:hackney, "~> 1.17"},
-      {:bypass, "~> 2.1"}
+      {:bypass, "~> 2.1"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.14.6"}
     ]
   end
 
