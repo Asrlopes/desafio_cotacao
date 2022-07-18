724d7221729e111125ffe8c81e43ea43680f74da
Amorésio <a.junior.srlopes@gmail.com>
2022-07-18 18:38:53 -0400

    add typespect and parser

--- lib/desafio_cotacao/http_client/service_a/api.ex
+++ lib/desafio_cotacao/http_client/service_a/api.ex
@@ -1,8 +1,10 @@
 defmodule DesafioCotacao.HttpClient.ServiceA.Api do
   use Tesla
 
+  alias DesafioCotacao.HttpClient.ServiceA.Body
   alias DesafioCotacao.HttpClient.ServiceA.Client
   alias DesafioCotacao.HttpClient.ServiceA.Response
+  alias DesafioCotacao.Utils.Parser
 
   @base_url "http://localhost:8080/servico-a"
 
@@ -25,6 +27,17 @@ defmodule DesafioCotacao.HttpClient.ServiceA.Api do
     end
   end
 
-  defp handle_get({:ok, %Tesla.Env{status: 200, body: body}}), do: {:ok, body}
+  defp handle_get({:ok, %Tesla.Env{status: 200, body: body}}), do: {:ok, normalize_body(body)}
   defp handle_get({:ok, %Tesla.Env{status: 400, body: body}}), do: {:ok, body}
+
+  @spec normalize_body(Body.t()) :: %{cotacao: integer(), currency: binary()}
+  defp normalize_body(%{"cotacao" => currency_value, "moeda" => currency, "symbol" => _symbol}) do
+    %{
+      "cotacao" => Parser.parse_float_to_int(currency_value),
+      "moeda" => currency
+    }
+  end
+
+  @spec normalize_body(Body.t()) :: %{error: binary()}
+  defp normalize_body(%{"erro" => _error_message} = error), do: error
 end
